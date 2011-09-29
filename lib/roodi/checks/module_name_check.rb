4aff319e4adaf267337ab79bc7e2885010431e95
Duncan Bayne <dhgbayne@gmail.com>
2011-09-30 08:25:07 +1000

    Now checks for naughty overrides, starting with 'class'.

--- lib/roodi/checks/module_name_check.rb
+++ lib/roodi/checks/module_name_check.rb
@@ -4,7 +4,7 @@ module Roodi
   module Checks
     # Checks a module name to make sure it matches the specified pattern.
     # 
-    # Keeping to a consistent nameing convention makes your code easier to read.
+    # Keeping to a consistent naming convention makes your code easier to read.
     class ModuleNameCheck < NameCheck
       DEFAULT_PATTERN = /^[A-Z][a-zA-Z0-9]*$/
       
